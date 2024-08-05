96e205471d1d4bb63f77abc8c1656f516a643005
Brian Nelson <briannelson@Brians-MacBook-Air.local>
2024-08-05 10:53:00 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Main template file
+ * 
+ * @package BrianNelson
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BrianNelson Wordpress Theme</title>
+</head>
+<body>
+    
+</body>
+</html>
